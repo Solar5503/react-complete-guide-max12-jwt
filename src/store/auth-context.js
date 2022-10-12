21c69b1c65fbc842d6f0aed09874625f0a155b4f
Solar5503 <spidervvv@mail.ru>
2022-10-12 15:57:07 +0300

    protecting pages and save auth in LS

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,11 +8,18 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
 
-  const loginHandler = (token) => setToken(token);
-  const logoutHandler = () => setToken(null);
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem('token', token);
+  };
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem('token');
+  };
 
   const contextValue = {
     token,
